30da91f3f13fdf771099a0135ab64ebef7bdd34f
lalastalin <lalanattida@gmail.com>
2022-06-28 04:16:43 +0700

    deploy fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import GlobalContexts from "./util/context";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+ReactDOM.render(
   <React.StrictMode>
     <GlobalContexts>
       <App />
     </GlobalContexts>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
